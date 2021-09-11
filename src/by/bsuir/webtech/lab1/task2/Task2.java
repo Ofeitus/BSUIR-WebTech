cc6bf2dbd916bc3329255714443156ad945f9cfc
Ofeitus <2008655z@gmail.com>
2021-09-11 16:58:02 +0300

    task 2

---
+++ src/by/bsuir/webtech/lab1/task2/Task2.java
@@ -0,0 +1,14 @@
+package by.bsuir.webtech.lab1.task2;
+
+import java.util.Scanner;
+
+public class Task2 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("x: ");
+        double x = input.nextInt();
+        System.out.print("y: ");
+        double y = input.nextInt();
+        System.out.print(0 <= y && y <= 5 && -4 <= x && x <= 4 || -3 <= y && y <= 0 && -6 <= x && x <= 6);
+    }
+}
