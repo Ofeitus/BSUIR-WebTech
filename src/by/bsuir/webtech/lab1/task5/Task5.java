00546dfd27b46a248e411203338024c8e3c6bddd
Ofeitus <2008655z@gmail.com>
2021-09-12 01:59:42 +0300

    task 5

--- src/by/bsuir/webtech/lab1/task5/Task5.java
+++ src/by/bsuir/webtech/lab1/task5/Task5.java
@@ -1,7 +1,31 @@
 package by.bsuir.webtech.lab1.task5;
 
+import java.util.Scanner;
+
 public class Task5 {
     public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("N: ");
+        int N = input.nextInt();
+        int[] A = new int[N];
+        for (int i = 0; i < N; i++) {
+            System.out.printf("Array[%d] = ", i);
+            A[i] = input.nextInt();
+        }
 
+        int[] d = new int[N];
+
+        for (int i = 0; i < N; ++i) {
+            d[i] = 1;
+            for (int j = 0; j < i; ++j)
+                if (A[j] < A[i])
+                    d[i] = Math.max(d[i], 1 + d[j]);
+        }
+
+        int max = d[0];
+        for (int i = 0; i < N; ++i)
+            max = Math.max(max, d[i]);
+
+        System.out.println("\nK = " + (N - max));
     }
 }
