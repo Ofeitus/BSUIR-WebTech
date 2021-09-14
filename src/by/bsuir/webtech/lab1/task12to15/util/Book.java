9a9451a58756ec9bf1ed05699a9a077d17120eaa
Ofeitus <2008655z@gmail.com>
2021-09-14 16:25:34 +0300

    task 15

--- src/by/bsuir/webtech/lab1/task12to15/util/Book.java
+++ src/by/bsuir/webtech/lab1/task12to15/util/Book.java
@@ -1,6 +1,8 @@
 package by.bsuir.webtech.lab1.task12to15.util;
 
-public class Book implements Cloneable, Comparable {
+import java.util.Comparator;
+
+public class Book implements Cloneable, Comparable<Book> {
     private String title;
     private String author;
     private int price;
@@ -50,8 +52,18 @@ public class Book implements Cloneable, Comparable {
     }
 
     @Override
-    public int compareTo(Object o) {
-        Book book = (Book)o;
+    public int compareTo(Book book) {
         return this.isbn - book.isbn;
     }
+
+    public static Comparator<Book> byAuthor = (book1, book2) -> book1.author.compareToIgnoreCase(book2.author);
+
+    public static Comparator<Book> byTitle = (book1, book2) -> book1.title.compareToIgnoreCase(book2.title);
+
+    public static Comparator<Book> byPrice = Comparator.comparingInt(book -> book.price);
+
+    public static Comparator<Book> byTitleThanAuthor = byTitle.thenComparing(byAuthor);
+    public static Comparator<Book> byAuthorThanTitle = byAuthor.thenComparing(byTitle);
+    public static Comparator<Book> byAuthorThanTitleThanPrice = byAuthor
+            .thenComparing(byTitle).thenComparing(byPrice);
 }
